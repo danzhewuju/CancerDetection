1ac1b92e93be0f22f9a2f73942d32c75ee0c5c02
alexanderdan <1668926294@qq.com>
2019-02-27 21:22:42 +0800

    数据预处理

--- PreDataProcessing.py
+++ PreDataProcessing.py
@@ -5,8 +5,9 @@ import numpy as np
 import torchvision
 import torch
 import torchvision.transforms as transforms
+import os
 
-path_label = "dataset/train_labels.csv/train_labels.csv"
+path_label = "dataset/train_labels.csv"
 
 
 def get_lables(path):                          #获取训练图片数据的labels
@@ -30,18 +31,29 @@ def read_img(path):
 
 
 def write_image_labels(path_write, path_labels):        #注意pytorch数据的加载方式，我们需要写成  “****.png 1” 这样的格式
+    index = 0
     dict_labels = get_lables(path_labels)
     f = open(path_write, 'w', encoding="UTF-8")
     for d in dict_labels.items():
-        name = '.'.join(d[0], 'tif')
-        
-
-
+        name = '.'.join([d[0], "tif"])
+        name_pre = "dataset/train"
+        path_name = os.path.join(name_pre, name)  #获得的完整路径
+        label = d[1]
+        result = path_name+" "+str(label)+"\n"
+        f.write(result)
+        index += 1
+        if index % 100 == 0:
+            print("Processing %d data" % index)
+    f.close()
+    return True
 
 
 # id_t = "f38a6374c348f90b587e046aac6079959adf3835"
 # data = get_lables(path_label)
 # print(data[id_t])
-path_image = "dataset/train"
-read_img(path_image)
+# path_image = "dataset/train"
+# read_img(path_image)
+path_labels="dataset/train_labels.csv"
+path_write="dataset/train_data_labels.txt"
+write_image_labels(path_write, path_labels)
 
--- tets.py
+++ tets.py
@@ -4,3 +4,7 @@ dict1 = {"1": "yuhao", "2": "nihao"}
 for a in dict1.items():
     print(a[1])
 
+a = "123"
+b = "456"
+print(".".join([a, b]))
+
