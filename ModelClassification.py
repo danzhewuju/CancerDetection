61f17cd194297a8feba58d31006c257cdf6acc19
Alex <1668926294@qq.com>
2019-03-04 10:09:44 +0800

    CNN result

--- ModelClassification.py
+++ ModelClassification.py
@@ -4,14 +4,20 @@
 '''
 import torch
 import glob
-from torch.utils.data import DataLoader, Dataset
-
-import torch.nn as nn
-
-from torchvision import datasets, models, transforms
-
 import os
-
+from torch.utils.data import DataLoader, Dataset
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+import torchvision
+from torchvision import datasets, models, transforms
+import time
+import os
+from PIL import Image
+import sys
+import torch.nn.functional as F
 from PIL import Image
 # device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -19,6 +25,8 @@ test_path = "dataset/test"  #验证集的位置
 mode_path = "./model/model.ckpt"  #训练好的模型的文件位置
 batch_size = 100
 num_classes = 2
+save_path = "./dataset/submit.csv"#生成需要提交的文件
+write_path = "./result.csv"
 
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -39,7 +47,14 @@ class ConvNet(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
         self.fc = nn.Linear(24 * 24 * 32, num_classes)  #注意参数的调整，神经网络的参数要保持一致
-
+     
+    def forward(self, x):
+        # x = x.view(x.size(0), -1)
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
 
 def default_loader(path):
     return Image.open(path).convert('RGB')
@@ -48,6 +63,17 @@ def default_loader(path):
 def get_all_path(path):  #获取这个文件夹下面的所有文件
     paths = glob.glob(os.path.join(path, "*.tif"))
     return paths
+
+
+
+def get_name_list(path):# 获取全部的名字列表
+    names = []
+    for root, dir, files in os.walk(path):
+        for f in files:
+            n = f.split('.')[0]
+            names.append(n)
+    return names
+
 
 
 class MyDataLoader(Dataset):
@@ -69,21 +95,38 @@ class MyDataLoader(Dataset):
         return self.paths.__len__()
 
 
-test_data = MyDataLoader(test_path, transform=transforms.ToTensor())
-test_loader = DataLoader(test_data,  batch_size=batch_size, shuffle=True)  #标准数据集的构造
+def run():
+    test_data = MyDataLoader(test_path, transform=transforms.ToTensor())
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True)  # 标准数据集的构造
 
-net_r = ConvNet(num_classes=num_classes).to(device)   #保持和之前的神经网络相同的结构
-net_r.load_state_dict(torch.load("model/model.ckpt"))
+    net_r = ConvNet(num_classes=num_classes).to(device)  # 保持和之前的神经网络相同的结构
+    net_r.load_state_dict(torch.load("model/model.ckpt"))
+    names = get_name_list(test_path)
+    results = []
 
-# Test the model
-net_r.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
-with torch.no_grad():
+    # Test the model
+    net_r.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
+    with torch.no_grad():
+        for images in test_loader:
+            images = images.to(device)
+            # images = images
+            # labels = labels
+            outputs = net_r(images)
+            _, predicted = torch.max(outputs.data, 1)
+            print(predicted)
+            for r in predicted:
+                results.append(r)
+    fp = open(write_path, 'w', encoding="UTF-8")
+    fp.write("id,label\n")
+    for index in names.__len__():
+        result = names[index] + "," + str(result[index])+"\n"
+        fp.write(result)
+    fp.close()
+    print("Successfully Write!!!")
 
-    for images in test_loader:
-        images = images.to(device)
-        outputs = net_r(images)
-        _, predicted = torch.max(outputs.data, 1)
-        print(predicted)
+
+
+run()
 
 
 
